15a56d3b199d6aed7c57f355203829f9e55a8fa7
Andres Bolivar <dre.bolivar@gmail.com>
2022-06-27 17:16:35 -0400

    clicks work, declares winner, able to restart at end of game, having issue declaring draw

--- js/app.js
+++ js/app.js
@@ -1,31 +1,98 @@
 ////////////////////////////////
 // Global Variables Here
-let playerTxt = document.getElementById('playerTxt')
-let resetBtn = document.getElementById('resetBtn')
-let boxes = Array.from(document.getElementsByClassName('box'))
+const tiles = document.querySelectorAll('.tile')
+const xPlayer = 'X'
+const oPlayer = 'O'
+let turn = xPlayer
 
-const player1 = 'X'
-const player2 = 'O'
-let currentPlayer = player1
-let spaces = Array(9).fill(null)
+const boardState = Array(tiles.length)
+boardState.fill(null)
 
-// ////////////////////////////////
-// // Functions For Game Logic Here
+//Elements
+const results = document.getElementById('finishGame')
+const resultsText = document.getElementById('finishText')
+const reset = (document.getElementById('restart').onclick = startNew)
 
-// ////////////////////////////////
-// // Event Listeners Here
-const startGame = () => {
-  boxes.forEach((box) => box.addEventListener('click', boxClicked))
+////////////////////////////////
+// Functions For Game Logic Here
+
+function tileClick(event) {
+  if (results.classList.contains('visible')) {
+    return
+  }
+
+  const tile = event.target
+  const tileNumber = tile.dataset.index
+
+  if (tile.innerText != '') {
+    return
+  }
+
+  if (turn === xPlayer) {
+    tile.innerText = xPlayer
+    boardState[tileNumber] = xPlayer
+    turn = oPlayer
+  } else {
+    tile.innerText = oPlayer
+    boardState[tileNumber] = oPlayer
+    turn = xPlayer
+  }
+  console.log(turn)
+  checkWinner()
 }
-function boxClicked(e) {
-  const id = e.target.id
 
-  if (!spaces[id]) {
-    spaces[id] = currentPlayer
-    e.target.innerText = currentPlayer
+function checkWinner() {
+  for (const winningCombination of winningCombinations) {
+    const combo = winningCombination.combo
+    const tileValue1 = boardState[combo[0]]
+    const tileValue2 = boardState[combo[1]]
+    const tileValue3 = boardState[combo[2]]
 
-    currentPlayer = currentPlayer == player1 ? player2 : player1
+    if (
+      tileValue1 != null &&
+      tileValue1 === tileValue2 &&
+      tileValue1 === tileValue3
+    ) {
+      gameOver()
+    }
   }
 }
-startGame()
-// ////////////////////////////////
+//draw
+function tieGame() {
+  text = 'Tie game? How whack'
+  results.className = 'visible'
+  resultsText.innerText = text
+}
+
+function gameOver() {
+  if (turn == 'O') {
+    text = 'Winner is X!'
+  } else {
+    text = 'Winner is O!'
+  }
+  results.className = 'visible'
+  resultsText.innerText = text
+}
+
+function startNew() {
+  results.className = 'hidden'
+  boardState.fill(null)
+  tiles.forEach((tile) => (tile.innerText = ''))
+  turn = xPlayer
+  results
+}
+
+const winningCombinations = [
+  { combo: [0, 1, 2] },
+  { combo: [3, 4, 5] },
+  { combo: [6, 7, 8] },
+  { combo: [0, 3, 6] },
+  { combo: [1, 4, 7] },
+  { combo: [2, 5, 8] },
+  { combo: [0, 4, 8] },
+  { combo: [2, 4, 6] }
+]
+////////////////////////////////
+// Event Listeners Here
+////////////////////////////////
+tiles.forEach((tile) => tile.addEventListener('click', tileClick))
